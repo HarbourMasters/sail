6c7158ad3f8fd93702d09f1d71d71f25aa4e780c
Garrett Cox <garrettjcox@gmail.com>
2023-11-17 13:19:27 -0600

    Add a few un-used arguments for the sake of the example

--- custom_sail.ts
+++ custom_sail.ts
@@ -2,7 +2,7 @@ import { Sail } from "./Sail.ts";
 
 const sail = new Sail();
 
-sail.on("chat", (message) => {
+sail.on("chat", (message, user) => {
   if (message.match(/!kick (\d+)/)) {
     if (onCooldown("kick", 300)) return;
     const strengthStr = message.match(/!kick (\d+)/)![1];
@@ -27,10 +27,13 @@ sail.on("chat", (message) => {
   }
 });
 
-sail.on("redeem", (reward) => {
+sail.on("redeem", (reward, message, user) => {
   if (reward === "878f54ca-b3ec-4acd-acc1-c5482b5c2f8e") {
     sail.console("reset");
   }
+});
+
+sail.on("bits", (bits, message, user) => {
 });
 
 const cooldownMap: Record<string, boolean> = {};
